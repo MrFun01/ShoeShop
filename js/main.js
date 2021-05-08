fe65bb5ff0d1534bf9641acfaf1a886dfb0799b8
asilbek <asilbek>
2021-05-08 20:02:25 +0500

    changed nav and added slider

--- js/main.js
+++ js/main.js
@@ -3,94 +3,78 @@ $(document).ready(function () {
         if ($(this).scrollTop()>50)
             $('.navbar-section').addClass('navbar-active');
         else $('.navbar-section').removeClass('navbar-active')
-    })
+    });
 });
 
+// $('.slider-main').css('height', `${$(window).height() - $('.navbar_section').outerHeight()}px`)
 
-// $('#myCarousel').owlCarousel({
-//     loop:true,
-//     margin:10,
-//     nav:true,
-//     autoplay:true,
-//     autoplayTimeout:3000,
-//     dots:false,
-//     autoplayHoverPause:false,
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         600:{
-//             items:1
-//         },
-//         1000:{
-//             items:1
-//         }
-//     }
-// });
-//
-// $('#iir').owlCarousel({
-//     loop: true,
-//     margin: 10,
-//     nav: true,
-//     dots:false,
-//     responsive: {
-//         0: {
-//             items: 1
-//         },
-//         300: {
-//             items: 2
-//         },
-//         600: {
-//             items: 3
-//         },
-//         900:{
-//             items: 3
-//         },
-//         1000: {
-//             items: 5
-//         }
-//     }
-// });
-//
-// $('#mycarouselfirst').owlCarousel({
-//     loop:true,
-//     margin:40,
-//     autoplay:true,
-//     autoplayTimeout:4000,
-//     autoplayHoverPause:true,
-//     dots: false,
-//     responsive:{
-//         0:{
-//             items:3
-//         },
-//         600:{
-//             items:3
-//         },
-//         1000:{
-//             items:3
-//         }
-//     }
-// });
-//
-// $('.owl-carousel-main').owlCarousel({
-//     loop:true,
-//     margin:10,
-//     autoplay:true,
-//     autoplayTimeout:3000,
-//     autoplayHoverPause:false,
-//     dots: false,
-//     responsive:{
-//         0:{
-//             items:3
-//         },
-//         600:{
-//             items:3
-//         },
-//         1000:{
-//             items: 5
-//         }
-//     }
-// });
+
+class SLIDER2 {
+    constructor(options) {
+        this.slider = document.querySelector(options.slider);
+        this.sliderLine = this.slider.querySelector('.sliders');
+        this.slides = this.sliderLine.children;
+
+        this.prev = document.querySelector('.slider__prev2');
+        this.next = document.querySelector('.slider__next2');
+
+        this.activeSlide = 0;
+        this.moveUnit = 100;
+
+        for (let i = 0; i < this.slides.length; i++) {
+            const currentSlide = this.slides[i];
+
+            if (i != this.activeSlide) {
+                currentSlide.style.transform = `translateY(${this.moveUnit}%)`;
+            }
+
+            if (i === this.slides.length - 1) {
+                currentSlide.style.transform = `translateY(-${this.moveUnit}%)`;
+            }
+
+        }
+
+        this.prev.addEventListener('click', () => this.move(this.prev));
+        this.next.addEventListener('click', () => this.move(this.next));
+    }
+
+    move(btn) {
+        let LeftOrRightBTN = btn == this.next ? this.moveUnit * -1 : this.moveUnit;
+
+        for (let i = 0; i < this.slides.length; i++) {
+            let currentSlide = this.slides[i];
+            currentSlide.style.transition = '0ms';
+            if (currentSlide != this.activeSlide) {
+                currentSlide.style.transform = `translateY(${LeftOrRightBTN * -1}%)`
+            }
+        }
+
+
+        this.slides[this.activeSlide].style = `transform: translateY(${LeftOrRightBTN}%)`
+        this.slides[this.activeSlide].style.transition = '500ms'
+
+
+        if (btn == this.next) {
+            this.activeSlide++;
+            if (this.activeSlide > this.slides.length - 1) {
+                this.activeSlide = 0;
+            }
+        } else if (btn == this.prev) {
+            this.activeSlide--;
+            if (this.activeSlide < 0) {
+                this.activeSlide = this.slides.length - 1;
+            }
+        }
+
+        this.slides[this.activeSlide].style.transform = `translateY(${0})`
+        this.slides[this.activeSlide].style.transition = '500ms'
+
+    }
+}
+
+const sliderNum2 = new SLIDER2({
+    slider: '.slider-main',
+})
 
 
 
